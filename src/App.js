3cb8cefe072f3d2f756ebd2e7dd21ff23c7ee838
Lferg98 <lachieferg98@hotmail.com>
2023-04-25 19:52:43 +1000

    added navbar & footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Portfolio from './components/Portfolio'
-import Resume from './components/Resume'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
@@ -17,6 +20,7 @@ function App() {
           <Route path ="contact" element={<Contact />} />
           <Route path ="resume" element={<Resume />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
