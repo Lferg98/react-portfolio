aba4b239afd7def557e875459607b5a1858484ff
Lferg98 <lachieferg98@hotmail.com>
2023-04-26 14:51:12 +1000

    added styling to home page and completed home page layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import InstagramIcon  from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 import "../styles/Footer.css";
 
 function Footer() {
@@ -11,6 +12,7 @@ function Footer() {
                 <InstagramIcon />
                 <FacebookIcon />
                 <LinkedInIcon />
+                <EmailIcon />
                 
             </div>
             <p>&copy; 2023 Lferg98.com</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from '@material-ui/icons/Email';
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Lachlan Ferguson</h2>
+        <div className="prompt">
+          <p>A web-development student with a passion for learning and creating.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills </h1>
+        <ol className="list">
+        <li className="item">
+          <h2> Front-End</h2>
+          <span>
+            HTML, CSS, Git, JavaScript, Bootstrap, The DOM, API's, JQuery, JSON, AJAX, Bulma, NPM, Yarn
+          </span>
+        </li>
+        <li className="item">
+          <h2>Back-End</h2>
+          <span>
+            NodeJS, ExpressJS, ApolloSever, MySQL, MongoDB, Sequelize, Testing
+          </span>
+        </li>
+        <li className="item">
+          <h2>Languages</h2>
+          <span>
+            JavaScript, TypeScript, HTML, CSS
+          </span>
+        </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
